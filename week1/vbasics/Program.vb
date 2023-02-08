0d5bcd2abbb762a1f634beb023aec746ddd73d6c
ccschiber <ccschiber@gmail.com>
2023-02-08 12:00:13 -0500

    vbasics practis

---
+++ week1/vbasics/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+' comment?
+Module Program
+    Sub Main(args As String())
+    dim greeting as String = "Hey City!"
+        Console.WriteLine("Hello World!")
+        Console.WriteLine(greeting)
+    End Sub
+End Module
